4c866d37b64d4653da1bf04f0d1aed8ab1d62fd6
z_zoor <z_zoor@08239170-4572-482a-b791-2244fd6e1e4b>
2015-10-08 13:23:20 +0000

    revert last commit.
    git-svn-id: https://repository.vs.cs.hs-rm.de/svn/sdds/sDDS/trunk@660 08239170-4572-482a-b791-2244fd6e1e4b

--- platform/riot/examples/hello-world/bin/native/cpu/tramp.d
+++
@@ -1,5 +0,0 @@
-/home/andreas/workspace/RIOT-2014.12/examples/hello-world/bin/native/cpu/tramp.o: \
- /home/andreas/workspace/RIOT-2014.12/cpu/native/tramp.S \
- /usr/include/stdc-predef.h
-
-/usr/include/stdc-predef.h:
