627d9d73865056c79bee0f5bfd33418dff95d48a
Kevin Sapper <mail@kevinsapper.de>
2015-11-30 19:17:37 +0100

    Problem: test are not running on every machine Solution: use tap interface for tests

--- sdds/test/test_linux/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rumpbake hw_generic test_linux.bin test_linux
-
-rumprun qemu -i -M 128 -I if,vioif,-net tap,script=no,ifname=tap0 -W if,inet6,static,fe80:2::5054:ff:fe12:3456/64 -- test_linux.bin
-rumprun qemu -i -M 10 -I if,vioif,-net tap,script=no,ifname=tap0 -W if,inet6,static,fe80:2::5054:ff:fe12:3456/64 -- test_linux.bin
