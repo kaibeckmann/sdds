4f90da6fa970525242fcd53dc7d213252963827a
Kevin Sapper <kevin.b.sapper@student.hs-rm.de>
2015-11-30 17:31:00 +0100

    Problem: Commited binary files by example Solution: Extend ignore rules and remove them

--- sdds/test/testrunner.sh
+++ sdds/test/testrunner.sh
@@ -15,6 +15,7 @@ for test in ./* ; do
             exit 1
         fi
         ./$BASENAME
+        make clean >/dev/null 2>&1
         if [ $? -ne 0 ]; then
             echo "FAILED"
             exit 1
