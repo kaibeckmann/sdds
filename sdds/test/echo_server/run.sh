f9e6756694c487a287a0767b8e0b24ff0e87cb81
Kevin Sapper <kevin.b.sapper@student.hs-rm.de>
2015-12-17 18:48:42 +0100

    Problem: Receive buffer was to small so many messages got dropped Solution: expanded receive buffer to 8k

--- sdds/test/echo_server/run.sh
+++ sdds/test/echo_server/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rumprun-bake hw_virtio echo_server.bin echo_server
 
+#rumprun iso -W if,inet6,static,fe80::5054:0ff:fe62:bf16/64 -- echo_server.bin
 #rumprun qemu -i -M 256     -I if,vioif,'-net vde,sock=/var/run/vde2/tap0.ctl'     -W if,inet6,auto -- echo_server.bin
 rumprun qemu -i -M 256     -I if,vioif,'-net vde,sock=/var/run/vde2/tap0.ctl'     -W if,inet6,static,fe80::5054:0ff:fe62:bf16/64 -- echo_server.bin
