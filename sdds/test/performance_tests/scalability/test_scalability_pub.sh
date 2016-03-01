b8234ec17cbb12ced2e76e1c0e3409ea2ce84acd
Olga Dedi <olga@dedi.de>
2016-03-01 17:29:36 +0100

    pi test script changes

--- sdds/test/performance_tests/scalability/test_scalability_pub.sh
+++ sdds/test/performance_tests/scalability/test_scalability_pub.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#scripts/./end_test.sh `basename $1` $2 &
-cd scalability/linux_scalability_pub/
+cd linux_scalability_pub/
 ./prepare_test.sh
 make clean
 ./generate.sh
 make all
+../scripts/./end_test.sh linux_scalability_pub $1 &
 ./linux_scalability_pub 
+make clean
 exit
--- sdds/test/performance_tests/scripts/prepare_test.sh
+++ sdds/test/performance_tests/scripts/prepare_test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-iface=eth0
-ip=$(ip addr show dev $iface | sed -e's/^.*inet6 \(fd[^ ]*\)\/.*$/\1/;t;d')
+dev=eth0
+iface=
+ip=$(ip addr show dev $dev | sed -e's/^.*inet6 \(fd[^ ]*\)\/.*$/\1/;t;d')
 sed s/{ip}/$ip/ template.xml | sed s/{iface}/$iface/ > sdds.xml
