6acf0ec499d3eb94429219da45e8b127cb7abbfa
Olga Dedi <olga@dedi.de>
2016-03-02 20:54:10 +0100

    pi scalability tests more dynamic

--- sdds/test/performance_tests/scalability/test_scalability_sub.sh
+++ sdds/test/performance_tests/scalability/test_scalability_sub.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-cd linux_scalability_sub/
+cd ~/sdds/sdds/test/performance_tests/scalability/linux_scalability_sub/
+echo -e "preparing sub test @$2"
 ./prepare_test.sh
-make clean
-./generate.sh
-make all
+make clean >/dev/null 2>&1
+./generate.sh >/dev/null 2>&1
+echo "building sub test @$2"
+make all >/dev/null 2> ../sub_make_error.log
 ../../scripts/./end_test.sh linux_scalability_sub $1 &
-./linux_scalability_sub 
-make clean
+echo "running sub test @$2"
+./linux_scalability_sub >/dev/null 2> ../sub.log
+echo "sub test @$2 completed"
+echo "cleaning up sub test $2"
+make clean >/dev/null 2>&1
+echo "done @$2"
 exit
