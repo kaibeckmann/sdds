2c505c2ff9fc44c755c0182010755fe1a20be165
Olga Dedi <olga@dedi.de>
2016-03-10 17:22:16 +0100

    Added shell scripts for running throughput test

---
+++ sdds/test/performance_tests/throughput/test_throughput_sub.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -lt 3 ]; then
+    echo "usage $0 <duration (min)> <msg_size> <iface> [local]"
+    exit
+fi
+
+
+duration=$1
+msg_size=$2
+iface=$3
+
+echo "clean throughput sub $duration $msg_size"
+cd ~/sdds/sdds/test/performance_tests/throughput/linux_throughput_sub
+rm -f *.log
+make clean > /dev/null 2>&1
+
+echo "prepare throughput sub $duration $msg_size"
+./prepare.sh $duration $msg_size $sub_ip $iface $5
+
+echo "make throughput sub $duration $msg_size"
+make_log="make_"$duration"_"$msg_size".log"
+make > /dev/null 2> $make_log
+
+echo "run throughput sub $duration $msg_size"
+log="print_"$duration"_"$msg_size".log"
+./linux_throughput_sub > $log 2>&1
+
+exit
