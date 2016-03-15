d575f972dd6ab9ef1227e2245805759c35ed50f3
Olga Dedi <olga@dedi.de>
2016-03-15 20:40:31 +0100

    Performance test results

--- sdds/test/performance_tests/scalability/eval_scalability.sh
+++ sdds/test/performance_tests/scalability/eval_scalability.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 host=(
+	pi20
+	pi19
+	pi18
+	pi17
+	pi16
+	pi15
+	pi14
+	pi13
+	pi12
+	pi11
+	pi10
+	pi09
+	pi08
+	pi07
 	pi06
 	pi05
 	pi04
@@ -8,12 +22,12 @@ host=(
 	pi01
     )
 
-mkdir ../eval_scalability
+mkdir eval_scalability
 for (( i=0; i<${#host[@]}; i++ )); do
 	echo "connect to ${host[$i]}"
-	scp pi@${host[$i]}:~/sdds/sdds/test/performance_tests/scalability_*.log ../eval_scalability/.	
+	scp pi@${host[$i]}:~/sdds/sdds/test/performance_tests/scalability_*.log eval_scalability/.	
 done
 
-for file in ../eval_scalability/*.pcapng; do
+for file in eval_scalability/*.pcapng; do
 	tshark -2 -r $file > "${file%.pcapng}.wslog"
 done
