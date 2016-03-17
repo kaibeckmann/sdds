ce1e70f04070fcca96f12b1b74b16426b917b4d4
Olga Dedi <olga@dedi.de>
2016-03-17 10:11:30 +0100

    measiring how long it takes to set a pin

--- sdds/test/performance_tests/tracepoint/contiki_tracepoint_calibrate/contiki_tracepoint_calibtrate.c
+++ sdds/test/performance_tests/tracepoint/contiki_tracepoint_calibrate/contiki_tracepoint_calibtrate.c
@@ -6,22 +6,24 @@ AUTOSTART_PROCESSES(&contiki_tracepoint_calibtrate);
 
 PROCESS_THREAD(contiki_tracepoint_calibtrate, ev, data)
 {
-	//static struct etimer g_wait_timer;
+	static struct etimer g_wait_timer;
 
 	PROCESS_BEGIN();
 
-	//static DDS_ReturnCode_t ret;
+	static DDS_ReturnCode_t ret;
 
 	if (sDDS_init() == SDDS_RT_FAIL) {
 		return 1;
 	}
 	Log_setLvl(5);
 
-    static bool_t x = true;
     for (;;) {
-        Trace_point(!x);
-		//etimer_set(&g_wait_timer, 1);
-		//PROCESS_YIELD_UNTIL(etimer_expired(&g_wait_timer));
+        Trace_point(0);
+        Trace_point(1);
+        Trace_point(0);
+
+		etimer_set(&g_wait_timer, 10);
+		PROCESS_YIELD_UNTIL(etimer_expired(&g_wait_timer));
     }
 
     PROCESS_END();
